0734b0f18014e528054fdb936d625e96995509e2
Daniel Schaefer <46212174+Daniel-SchaeferJ@users.noreply.github.com>
2020-08-14 22:13:31 -0600

    Updated Gitnore and Made the Query to Biance Class

---
+++ src/main/java/com/univocity/shopify/controllers/QueryADAPriceAgainstMajorCurrencies.java
@@ -0,0 +1,12 @@
+package com.univocity.shopify.controllers;
+
+import java.util.Currency;
+public class QueryADAPriceAgainstMajorCurrencies
+{
+    public static Currency GetCardanoPriceInUSD()
+    {
+        return null;
+
+    }
+
+}
