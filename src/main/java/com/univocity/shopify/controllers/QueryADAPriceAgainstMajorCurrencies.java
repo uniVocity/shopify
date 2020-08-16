09a0c263f8155eb29f86db67c116ffecaa0c6129
Daniel Schaefer <46212174+Daniel-SchaeferJ@users.noreply.github.com>
2020-08-16 00:00:21 -0600

    Finished the Biance Method and Created the Interface

--- src/main/java/com/univocity/shopify/controllers/QueryADAPriceAgainstMajorCurrencies.java
+++ src/main/java/com/univocity/shopify/controllers/QueryADAPriceAgainstMajorCurrencies.java
@@ -1,12 +1,55 @@
 package com.univocity.shopify.controllers;
 
-import java.util.Currency;
-public class QueryADAPriceAgainstMajorCurrencies
-{
-    public static Currency GetCardanoPriceInUSD()
-    {
-        return null;
+import com.binance.api.client.BinanceApiClientFactory;
+import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.domain.market.TickerPrice;
+import com.binance.api.client.exception.BinanceApiException;
 
+import java.util.logging.Logger;
+
+
+public class QueryADAPriceAgainstMajorCurrencies implements ICardanoExchangeRatesToMajorCurrencies {
+    private final BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance();
+    private final BinanceApiRestClient client = factory.newRestClient();
+
+    //Queries the Binance Exchange using the Java Binance API. Gets the current Cardano Price
+    //and the method then converts it and returns a useable double. 
+    @Override
+    public double GetCardanoPriceInUSD() {
+        String cardanoPrice;
+        try {
+            TickerPrice cardanoTickerPrice = client.getPrice("ADA");
+            cardanoPrice = cardanoTickerPrice.getPrice();
+            return Double.parseDouble(cardanoPrice);
+        } catch (BinanceApiException e) {
+            client.ping();
+            long response = client.getServerTime();
+            if (response == 0L) {
+                throw new TypeNotPresentException("CardanoValue",e.getCause());
+            }
+            e.getMessage();
+
+        } catch (Exception e) {
+            e.getMessage();
+        } finally {
+            return 0.0;
+        }
+
+    }
+
+    @Override
+    public double GetCardanoPriceInGBP() {
+        return 0.0;
+    }
+
+    @Override
+    public double GetCardanoPriceInEUR() {
+        return 0.0;
+    }
+
+    @Override
+    public double GetCardanoPriceInAUD() {
+        return 0.0;
     }
 
 }
