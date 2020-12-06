4b98fa34865c0ccce2bd610766f127ac3349ef9e
Daniel Schaefer <46212174+Daniel-SchaeferJ@users.noreply.github.com>
2020-12-06 11:52:33 -0800

    Addint integration tests

--- src/main/java/com/univocity/shopify/price/HitBtcPriceConverter.java
+++ src/main/java/com/univocity/shopify/price/HitBtcPriceConverter.java
@@ -1,4 +1,9 @@
 package com.univocity.shopify.price;
 
-public class HitBtcPriceConverter {
+public class HitBtcPriceConverter implements PriceConverter{
+    @Override
+    public double getLatestPrice(String tokenSymbol, String currencySymbol) {
+        return 0;
+        //https://api.hitbtc.com/api/2/public/ticker/ADAUSD
+    }
 }
